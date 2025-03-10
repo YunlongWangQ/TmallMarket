3053f12dd0797c5f8f1273b3a46d25711123373f
wangyl <3052352869@qq.com>
2025-03-10 22:11:01 +0800

    B#20250310_001:测试用

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("eaesfas");
+        list.add("sdasadd");
+        for(String s : list){
+            System.out.println(s);
+        }
+    }
+}
